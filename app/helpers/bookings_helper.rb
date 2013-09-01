6d5c02f888f08b7dae8e4e543ca92e844757bea7
Stuart Varrall <github@fpstudios.com>
2013-09-01 11:25:27 +0100

    to pay link now takes into account player type (junior etc.)
    Need to add all the classes (ie. colours)

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,11 +1,14 @@
 module BookingsHelper
 
-def paid_link_text(payable)  
-  payable.paid? ? 'Paid' : 'Pay'  
+def paid_link_text(booking)  
+  booking.paid? ? 'Paid' : 'Pay'  
 end 
 
-def paid_link_class(payable)  
-  payable.paid? ? 'has_paid' : 'to_pay'  
+def paid_link_class(booking)
+  @player = Player.find(booking.player_id)
+  @membership_type = MembershipType.find(@player.membership_type_id)
+  
+  booking.paid? ? 'has_paid' : 'to_pay'+ '_' + @membership_type.membership_type
 end
 
 end
