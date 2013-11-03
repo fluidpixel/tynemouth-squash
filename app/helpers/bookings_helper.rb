5bede623e7d9f4cef900e399f020d8021d92274d
Stuart Varrall <github@fpstudios.com>
2013-11-03 23:31:25 +0000

    to pay updates and trial added as special case
    Needs changing from trial to class?

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -12,6 +12,13 @@ def paid_link_class(booking)
   booking.paid? ? 'has_paid' : 'to_pay'+ '_' + @membership_type.membership_type
 end
 
+def membership_type(booking)
+  @player = Player.find(booking.player_id)
+  @membership_type = MembershipType.find(@player.membership_type_id + 1)
+  
+  @membership_type.membership_type
+end
+
 def membership_type_and_price(booking)
   @player = Player.find(booking.player_id)
   @membership_type = MembershipType.find(@player.membership_type_id + 1)
