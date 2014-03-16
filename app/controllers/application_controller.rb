afaf3b9f8c25011d53147cb7b2766faf14b730db
Stuart Varrall <github@fpstudios.com>
2014-03-16 22:59:02 +0000

    prevent i.e. caching site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,15 @@ class ApplicationController < ActionController::Base
   helper_method :is_super_admin
   helper_method :is_bank_holiday
   
+  before_filter :set_cache_buster
+  
   private
+  
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
   
   def current_player
    if (session[:player_id])
