74c1e099f1e31aa128387d722ab03350112ff2db
Stuart Varrall <github@fpstudios.com>
2014-06-27 09:54:48 +0100

    (new) cancelled court to booking

--- db/migrate/20130805071418_create_players.rb
+++ db/migrate/20130805071418_create_players.rb
@@ -3,12 +3,17 @@ class CreatePlayers < ActiveRecord::Migration
     create_table :players do |t|
       t.string :first_name
       t.string :last_name
-      t.string :telephone
+      t.string :landline
+      t.string :mobile
       t.string :email
       t.string :membership_number
       t.integer :membership_type_id
       t.boolean :admin
-      
+      t.datetime :trial_date
+      t.string   :address_line1
+      t.string   :address_line2
+      t.string   :address_line3
+      t.string   :post_code
       t.timestamps
     end
   end
