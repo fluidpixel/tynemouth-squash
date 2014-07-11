f0ada33392e8b741a043c0e1fa38c19ac8a32011
Stuart Varrall <github@fpstudios.com>
2014-07-11 23:46:38 +0100

    (new) bookings cost for vs courts are charged by lowest membership type

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -9,6 +9,17 @@ def paid_link_class(booking)
   @player = Player.find(booking.player_id)
   @membership_type = MembershipType.find(@player.membership_type_id)
   
+  if booking.vs_player_id
+    @vsplayer = Player.find(booking.vs_player_id)
+    @vs_membership_type = MembershipType.find(@vsplayer.membership_type_id)
+    
+    if @vs_membership_type.court_cost < @membership_type.court_cost
+      @membership_type = @vs_membership_type
+    end
+    
+  end
+  
+  
   booking.paid? ? 'has_paid' : 'to_pay'+ '_' + @membership_type.membership_type
 end
 
@@ -16,12 +27,31 @@ def membership_type(booking)
   @player = Player.find(booking.player_id)
   @membership_type = MembershipType.find(@player.membership_type_id)
   
+  if booking.vs_player_id
+    @vsplayer = Player.find(booking.vs_player_id)
+    @vs_membership_type = MembershipType.find(@vsplayer.membership_type_id)
+    
+    if @vs_membership_type.court_cost < @membership_type.court_cost
+      @membership_type = @vs_membership_type
+    end
+  end
+  
   @membership_type.membership_type
 end
 
 def membership_type_and_price(booking)
+  
   @player = Player.find(booking.player_id)
   @membership_type = MembershipType.find(@player.membership_type_id)
+  
+  if booking.vs_player_id
+    @vsplayer = Player.find(booking.vs_player_id)
+    @vs_membership_type = MembershipType.find(@vsplayer.membership_type_id)
+    
+    if @vs_membership_type.court_cost < @membership_type.court_cost
+      @membership_type = @vs_membership_type
+    end
+  end
   
   @membership_type.membership_type + ' ' + number_to_currency(@membership_type.court_cost, :unit => "£")
 end
