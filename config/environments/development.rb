563d7a490b83b22aacb86789f46da3d240ccce39
Stuart Varrall <github@fpstudios.com>
2015-02-12 22:03:24 +0000

    (new) don't send emails in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,9 @@ Squash::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = false
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
