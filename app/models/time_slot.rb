299a8767c61335b5c52edf231c0d0b167a15b8dd
Stuart Varrall <git@fpstudios.com>
2015-09-10 22:22:56 +0100

    (fix) css for peak courts

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -2,8 +2,9 @@ class TimeSlot < ActiveRecord::Base
 	belongs_to :court
 	has_many :bookings
   
-  def is_peak
-    if self.time.hour >= 16 && self.time.hour <= 20
+  def is_peak(day)
+    
+    if self.time.hour >= 16 && self.time.hour <= 20 && !(Date.current + day.days).saturday? && !(Date.current + day.days).sunday?
       return true
     else
       return false
