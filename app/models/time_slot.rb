c74a6e3b34a67bd57990f5208abbb216558ef282
Stuart Varrall <git@fpstudios.com>
2015-09-10 22:14:29 +0100

    (new) peak times highlighted

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,4 +1,13 @@
 class TimeSlot < ActiveRecord::Base
 	belongs_to :court
 	has_many :bookings
+  
+  def is_peak
+    if self.time.hour >= 16 && self.time.hour <= 20
+      return true
+    else
+      return false
+    end
+  end
+  
 end
