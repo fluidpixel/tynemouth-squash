db286f67aa266402c3a51885fa3a64ac47c018ff
Stuart Varrall <git@varrall.com>
2020-08-13 09:07:01 +1000

    update for new gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ui/autocomplete
-//= require jquery-ui/datepicker
+//= require jquery-ui/widgets/autocomplete
+//= require jquery-ui/widgets/datepicker
 //= require jquery_ujs
 //= require_tree .
 
