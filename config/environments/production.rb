f644cbf7b834cf185e703e3bb50ba113d26ce92a
Stuart Varrall <git@varrall.com>
2020-08-23 20:15:49 +1000

    stop warnings for rails5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,12 @@ Squash::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
