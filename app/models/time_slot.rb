8e8c2d5ef09f0d2b787477e1680fddd1e62ed0c4
Stuart Varrall <github@fpstudios.com>
2025-03-13 23:22:30 +1000

    added additional peak hours to weekend
    courts 1 and 2 before 12

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -2,9 +2,12 @@ class TimeSlot < ActiveRecord::Base
 	belongs_to :court
 	has_many :bookings
   
-  def is_peak(day)
-    
-    if self.time.hour >= 16 && self.time.hour <= 20 && !(Date.current + day.days).saturday? && !(Date.current + day.days).sunday?
+  def is_peak(day, court_id)
+    dayOfWeek = Date.current + day.days
+
+    if self.time.hour >= 16 && self.time.hour <= 20 && !dayOfWeek.saturday? && !dayOfWeek.sunday?
+      return true
+    elsif (court_id == 1 || court_id == 2) && self.time.hour < 12 && (dayOfWeek.saturday? || dayOfWeek.sunday?)
       return true
     else
       return false
